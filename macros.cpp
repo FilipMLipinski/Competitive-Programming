a888a45373da5ffd18874f50dbc0838fb2d7af7e
FilipMLipinski <72980477+FilipMLipinski@users.noreply.github.com>
2024-08-25 19:55:14 +0300

    updated macros

--- macros.cpp
+++ macros.cpp
@@ -1,30 +1,20 @@
 #include <string>
 #include <vector>
+#include <set>
+#include <unordered_map>
 #include <iostream>
 
-#define REP(i, n) for (int i = 0; (i) < (int)(n); ++ (i))
-#define REP_R(i, n) for (int i = (int)(n) - 1; (i) >= 0; -- (i))
 using namespace std;
 
-const string YES = "YES";
-const string NO = "NO";
-bool solve(const vector<int> & a, string & b) {
+bool solve() {
     // TODO: edit here
 }
 
 int main() {
-    int n;
-    cin >> n;
-    REP (i, n) {
-        int k;
-        cin >> k;
-        vector<int> a(k);
-        REP(j, k){
-            cin >> a[j];
-        }
-        string b;
-        cin >> b;
-        cout << (solve(a,b) ? "YES":"NO") << endl;
+    int t;
+    cin >> t;
+    for(int i = 0; i<t; i++) {
+
     }
     return 0;
 }
