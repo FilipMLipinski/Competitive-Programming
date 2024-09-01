39fdc5be1d9d4eb972ff2b348ea0d6c01ed134ed
FilipMLipinski <72980477+FilipMLipinski@users.noreply.github.com>
2024-09-01 16:19:46 +0300

    updated 962 D

--- Codeforces/Round 962 (Div 3)/D.cpp
+++ Codeforces/Round 962 (Div 3)/D.cpp
@@ -24,12 +24,29 @@ ll permutations(int a, int b, int c){
     return 6;
 }
 
+bool cond(int a, int b, int n, int x){
+    return min(x-a-b, (n-a*b)/(a+b)) >= b;
+}
 
+int getmaxb(int a, int x, int n){
+    int e = x-a;
+    int s = a;
+    // max b is somewhere in [s, e)
+    while(s+1!=e){
+        int m = s + (e-s)/2;
+        if(cond(a,m,n,x)){
+            s = m;
+        }
+        else e = m;
+    }
+    return s;
+}
 
 ll solve(int n, int x) {
     ll count = 0;
-    for(int a=1;a<x-1;a++){
-        for(int b=a;b<x-1;b++){
+    for(int a=1;a<=x/3;a++){
+        int maxb = getmaxb(a,x,n);
+        for(int b=a;b<=maxb;b++){
             int maxc = min((n-a*b)/(a+b), x-a-b);
             // count all permutations (a,b,c) where b<=c<=maxc;
             // first check if any such permutation exists;
